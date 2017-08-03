26851e27e10dbf0397f4abe2d16c7a44e9ecb2bd
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-08-03 09:13:22 -0400

    3rd exercise in elementary-electron

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ var picture = require('cat-picture')
 var src = picture.src
 picture.remove()
 var image = require('lightning-image-poly')
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'})
